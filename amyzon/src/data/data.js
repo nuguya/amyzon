833c1cd6d5329380ee476e2c4063bd3afa0d2964
Geunyoung Kim <emsud@inu.ac.kr>
2019-09-19 00:02:25 +0900

    Add : add init data file
    초기화 데이터 파일 설정. 객체배열의 형태로 카테고리 내용과 그에 따른 캐러셀에 들어갈 컨텐츠를 받아오기 위해서 만들었다.

---
+++ amyzon/src/data/data.js
@@ -0,0 +1,89 @@
+const cardlist = [
+  {
+    name: "Ship",
+    feature: [
+      {
+        title: "신속배송",
+        header: "아 귀찮아",
+        text: "너무많다",
+        navi: "asdfaf"
+      },
+      {
+        title: "정확한",
+        header: "진심 개많네",
+        text: "짜증나",
+        navi: "asdfafasdf"
+      }
+    ]
+  },
+  {
+    name: "Stream",
+    feature: [
+      {
+        title: "스트리밍",
+        header: "아 귀찮아",
+        text: "너무많다",
+        navi: "asdfaf"
+      },
+      {
+        title: "굿",
+        header: "진심 개많네",
+        text: "짜증나",
+        navi: "asdfafasdf"
+      }
+    ]
+  },
+  {
+    name: "Shop",
+    feature: [
+      {
+        title: "GS25",
+        header: "아 귀찮아",
+        text: "너무많다",
+        navi: "asdfaf"
+      },
+      {
+        title: "fuck you",
+        header: "진심 개많네",
+        text: "짜증나",
+        navi: "asdfafasdf"
+      }
+    ]
+  },
+  {
+    name: "Read",
+    feature: [
+      {
+        title: "책을 읽읍시다.",
+        header: "아 귀찮아",
+        text: "너무많다",
+        navi: "asdfaf"
+      },
+      {
+        title: "안녕하세요",
+        header: "진심 개많네",
+        text: "짜증나",
+        navi: "asdfafasdf"
+      }
+    ]
+  },
+  {
+    name: "More",
+    feature: [
+      {
+        title: "드디어 끝",
+        header: "아 귀찮아",
+        text: "너무많다",
+        navi: "asdfaf"
+      },
+      {
+        title: "이제 그만",
+        header: "진심 개많네",
+        text: "짜증나",
+        navi: "asdfafasdf"
+      }
+    ]
+  }
+];
+
+module.exports = cardlist;
