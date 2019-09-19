598fc365876059af9b50a3470689a95630cead59
Geunyoung Kim <emsud@inu.ac.kr>
2019-09-19 14:12:38 +0900

    Add : add function that set background image to individual card
    이미지 데이터를 불러와서 각각의 카테고리를 구성하는 카드에 배경이미지를 삽입해주는 함수 작성
    각각의 배경이미지를 넣을 요소와 불러들인 이미지 데이터를 인자값ㅇ로 받고 이를 연결한다. 만약 불러온 이미지의 개수와 컨테이너의 수가 다르다면 false를 반환한다.

--- amyzon/src/javascript/component/cardlist.js
+++ amyzon/src/javascript/component/cardlist.js
@@ -1,7 +1,5 @@
 const cardListView = cardName => {
-  return `<div class="card-category" id=${cardName}-card>
-    
-  </div>`;
+  return `<div class="card-category" id="${cardName}-card" role="tablist"><div class="${cardName}-card__clickable"><div class="${cardName}-card__title"></div></div><div class="${cardName}-card__pagination"></div></div>`;
 };
 
 module.exports = cardListView;
--- amyzon/src/javascript/index.js
+++ amyzon/src/javascript/index.js
@@ -1,12 +1,14 @@
+require("../style/style.scss");
 const cardList = require("../data/data");
 const cardListView = require("./component/cardlist");
 
 (function() {
   const main = document.querySelector(".contents");
   const { primeContainer, categoryCard, crousellContainer } = buildMain(main);
-  const { cardNames, cardDetails } = getCardListValue(cardList);
+  const { cardNames, cardDetails, cardImages } = getCardListValue(cardList);
 
   renderCardUi(categoryCard, cardListView, cardNames);
+  setCardImage(categoryCard.childNodes, cardImages);
 })();
 
 function buildMain(main) {
@@ -14,9 +16,9 @@ function buildMain(main) {
   const categoryCard = document.createElement("div");
   const crousellContainer = document.createElement("div");
 
-  primeContainer.classList.add("contents__croushell-container");
-  categoryCard.classList.add("contents__croushell-container__category-card");
-  crousellContainer.classList.add("contents__croushell-container__croushell");
+  primeContainer.classList.add("contents__primecroushell-container");
+  categoryCard.classList.add("contents__primecroushell-container__category-card");
+  crousellContainer.classList.add("contents__primecroushell-container__croushell");
 
   primeContainer.appendChild(categoryCard);
   primeContainer.appendChild(crousellContainer);
@@ -36,7 +38,11 @@ function getCardListValue(cardListData) {
   const cardDetails = cardListData.map(cur => {
     return cur.feature;
   });
-  return { cardNames, cardDetails };
+  const cardImages = cardListData.map(cur => {
+    return cur.image;
+  });
+
+  return { cardNames, cardDetails, cardImages };
 }
 
 function renderCardUi(target, view, data) {
@@ -49,4 +55,12 @@ function renderCardUi(target, view, data) {
 
 function render(target, component) {
   target.innerHTML = component;
+}
+
+function setCardImage(categoryCard, cardImages) {
+  let idx = 0;
+  if (categoryCard.length !== cardImages.length) return false;
+  categoryCard.forEach(element => {
+    element.childNodes[0].style.backgroundImage = `url("${cardImages[idx++]}")`;
+  });
 }
